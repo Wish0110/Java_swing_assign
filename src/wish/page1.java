9c3a7b85ca4f994f2c2fb172eb7f5cced3668e9c
Wish0110 <113825759+Wish0110@users.noreply.github.com>
2024-03-20 20:05:10 +0530

    lbl

--- src/wish/page1.java
+++ src/wish/page1.java
@@ -54,7 +54,7 @@ public class page1 extends JFrame {
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.CENTER;  // Center horizontally
-        gbc.insets = new Insets(0, 0, 400, 0);  // Top inset of 100px
+        gbc.insets = new Insets(0, 0, 450, 0);  // Top inset of 100px
         this.add(wallColorLabel, gbc);
 
         // Add spacing between labels (adjust as needed)
